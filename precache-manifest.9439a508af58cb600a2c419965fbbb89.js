d4aded96b848ea0f1c34178ca840d14a9920307e
Alejandro Martin Perez <alexandermpf77@gmail.com>
2019-12-31 17:41:46 +0100

    Updates

---
+++ precache-manifest.9439a508af58cb600a2c419965fbbb89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1b2bf75e51669ee17c8f6613618f23",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "8428e58865d0d0903a16",
+    "url": "/weather-app-react/static/css/main.3a40f9bb.chunk.css"
+  },
+  {
+    "revision": "899c5420d633f8dc3a2d",
+    "url": "/weather-app-react/static/js/2.ac4c9bd0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app-react/static/js/2.ac4c9bd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8428e58865d0d0903a16",
+    "url": "/weather-app-react/static/js/main.dbbf4259.chunk.js"
+  },
+  {
+    "revision": "545114453c451da5f1ef",
+    "url": "/weather-app-react/static/js/runtime-main.0a994585.js"
+  },
+  {
+    "revision": "18533a59a55bdd2a1734156035a08768",
+    "url": "/weather-app-react/static/media/sunny-weather.18533a59.jpeg"
+  }
+]);
